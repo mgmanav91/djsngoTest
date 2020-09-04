25dfd0653ba2633d27f2b462ec9d4effcc2e6679
mgmanav91 <gmanav.91@gmail.com>
2020-09-04 22:43:14 +0530

    new project

--- Stocks/config.py
+++
@@ -1,5 +0,0 @@
-status_matrix = {'request_stock_item_confirmed': ['request_stock_item_placed', 'request_stock_item_confirmed'],
-        'request_stock_item_shipped': ['request_stock_item_confirmed'],
-        'request_stock_item_delivered': ['request_stock_item_shipped'],
-        'request_stock_item_delivery_denied': ['request_stock_item_shipped'],
-        'request_stock_item_cancelled': ['request_stock_item_placed']}
